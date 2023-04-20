4f259bd67582e156fb32230b5a89579bc0730f46
Randa Natras <64141673+Randa-lab@users.noreply.github.com>
2023-04-20 19:22:53 +0200

    Script for plotting time-series data

---
+++ plot_timeseries.py
@@ -0,0 +1,5 @@
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
