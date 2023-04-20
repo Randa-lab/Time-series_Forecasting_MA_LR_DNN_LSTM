9d1bcbd57df4bc7e6ebb2bc22d7d2342ddb31d09
Randa Natras <64141673+Randa-lab@users.noreply.github.com>
2023-04-20 19:21:52 +0200

    Script for generating time-series data
    Script for generating time-series data with trend, seasonality and noise.

---
+++ generate_data.py
@@ -0,0 +1,17 @@
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.6,
+                    np.cos(season_time * 3 * np.pi),
+                    1 / np.exp(2 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
